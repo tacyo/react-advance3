5bf2ebc88f7ff5be442b075f1d6c1a110db05e9c
tacyo <107659547+tacyo@users.noreply.github.com>
2022-06-23 11:21:54 +0900

    finish

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,16 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  const history = useHistory();
+  console.log(state);
+  const onClickBack = () => {
+    history.goBack();
+  };
   return (
     <div>
       <h1>Page1DetailAです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
